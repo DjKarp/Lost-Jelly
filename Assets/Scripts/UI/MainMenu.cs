ca140b046cee615c0908f037b7d6c592e7bae8da
redleggames <redleggames@gmail.com>
2025-04-16 11:27:31 +0700

    - Added Settings, About screens; - Draw for this windows fon;

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -6,11 +6,11 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private Button _startGameplayButton;
-
     [SerializeField] private Button _levelSelectButton;
     [SerializeField] private GameObject _levelSelectRootGO;
-    private LevelSelect m_LevelSelect;    
+    [SerializeField] private LevelSelect m_LevelSelect;
+    [SerializeField] private Settings m_Settings;
+    [SerializeField] private Title m_Title;
 
     [SerializeField] private Image _fonImg;
     [SerializeField] private Sprite _mainMenuImage;
@@ -18,7 +18,6 @@ public class MainMenu : MonoBehaviour
 
     public void Initialize(bool isLevelSelect = false)
     {
-        m_LevelSelect = _levelSelectRootGO.GetComponent<LevelSelect>();
         if (isLevelSelect)
             m_LevelSelect.Initialize(this);
 
@@ -33,9 +32,23 @@ public class MainMenu : MonoBehaviour
 
     public void SwitchWindows(bool isLevelSelect = false)
     {
-        _startGameplayButton.gameObject.SetActive(!isLevelSelect);
         _levelSelectRootGO.SetActive(isLevelSelect);
+        m_Settings.gameObject.SetActive(false);
         _fonImg.sprite = isLevelSelect ? _levelSelectImage : _mainMenuImage;
         _levelSelectButton.gameObject.SetActive(!isLevelSelect);
+    }
+
+    public void ShowHideSettings()
+    {
+        bool isShowedSettings = m_Settings.gameObject.activeSelf;
+        m_Settings.gameObject.SetActive(!isShowedSettings);
+        _fonImg.sprite = !isShowedSettings ? _levelSelectImage : _mainMenuImage;
+    }
+
+    public void ShowHideTitle()
+    {
+        bool isShowedTitle = m_Title.gameObject.activeSelf;
+        m_Title.gameObject.SetActive(!isShowedTitle);
+        _fonImg.sprite = !isShowedTitle ? _levelSelectImage : _mainMenuImage;
     }
 }
